41cb90d2371b745b25847b3ad13ac520a3474db9
John <wwwdotphilip@gmail.com>
2018-04-27 01:46:34 +0800

    refactor benefits presenter.

--- app/src/main/java/app/trial/eaglequotetrial/presenter/BenefitsPresenter.java
+++ app/src/main/java/app/trial/eaglequotetrial/presenter/BenefitsPresenter.java
@@ -2,14 +2,11 @@ package app.trial.eaglequotetrial.presenter;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.text.SpannableStringBuilder;
-import android.text.Spanned;
-import android.text.style.ForegroundColorSpan;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Switch;
 
@@ -34,6 +31,7 @@ public class BenefitsPresenter {
     private Spinner renewable;
     private BenefitsCallback mBenefitsCallback;
     private Benefits mBenefits;
+    private AlertDialog mAlertDialog;
 
     public void loadDialog(Activity activity, FancyButton fancyButton,
                            List<BenefitIcon> benefitIcons, Benefits benefits) {
@@ -45,105 +43,22 @@ public class BenefitsPresenter {
             }
         }
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-        builder.setTitle(customText(fancyButton.getText().toString(), Color.WHITE));
         if (icon != 0) {
             builder.setIcon(activity.getResources().getDrawable(icon));
         }
 
         loadBenefitsUI(activity, builder, fancyButton, benefitIcons);
 
-        AlertDialog alertDialog = builder.create();
-        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(activity.getResources().getColor(R.color.app_blue2_transparent)));
-        alertDialog.setOnShowListener(dialog -> {
-            alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.WHITE);
-            alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.WHITE);
-        });
-        alertDialog.show();
-    }
-
-
-    private SpannableStringBuilder customText(String text, int color) {
-        // Initialize a new spannable string builder instance
-        ForegroundColorSpan foregroundColorSpan = new ForegroundColorSpan(color);
-        SpannableStringBuilder ssBuilderText = new SpannableStringBuilder(text);
-
-        // Apply the text color span
-        ssBuilderText.setSpan(
-                foregroundColorSpan,
-                0,
-                text.length(),
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-        );
-        return ssBuilderText;
+        mAlertDialog = builder.create();
+        mAlertDialog.show();
     }
 
     private void loadBenefitsUI(Activity activity, AlertDialog.Builder builder,
                                 FancyButton fancyButton, List<BenefitIcon> benefitIcons) {
 
         LayoutInflater inflater;
-        View dialogView;
+        View dialogView = null;
         inflater = activity.getLayoutInflater();
-
-        builder.setNegativeButton(customText("Cancel", Color.WHITE), (dialog, which) -> {
-
-        });
-
-        builder.setNeutralButton(customText("Clear", Color.WHITE), (dialog, which) -> {
-            AlertDialog.Builder confirmation = new AlertDialog.Builder(activity);
-            confirmation.setTitle("Are you sure?");
-            confirmation.setIcon(activity.getResources().getDrawable(R.drawable.ic_error));
-            confirmation.setMessage("You cannot revert once you clear this content.");
-            confirmation.setNegativeButton("No", (dialog12, which12) -> {
-                loadDialog(activity, fancyButton, benefitIcons, mBenefits);
-            });
-            confirmation.setPositiveButton("Yes", (dialog1, which1) -> {
-                updateResources(activity, fancyButton, false, benefitIcons);
-                switch (fancyButton.getId()) {
-                    case R.id.fbHealthCover:
-                        mBenefits.healthCover = null;
-                        if (mBenefitsCallback != null){
-                            mBenefitsCallback.onHealthCoverUpdate(null);
-                        }
-                        break;
-                    case R.id.fbLifeCover:
-                        mBenefits.lifeCover = null;
-                        if (mBenefitsCallback != null){
-                            mBenefitsCallback.onLifeCoverUpdate(null);
-                        }
-                        break;
-                }
-            }).show();
-        });
-
-        builder.setPositiveButton(customText("Apply Changes", Color.WHITE), (dialog, which) -> {
-            updateResources(activity, fancyButton, true, benefitIcons);
-            switch (fancyButton.getId()) {
-                case R.id.fbHealthCover:
-                    HealthCover healthCover = new HealthCover();
-                    healthCover.excess = excess.getSelectedItem().toString();
-                    healthCover.dentalAndOptical = dental.isChecked();
-                    healthCover.gpAndPrescriptions = prescription.isChecked();
-                    healthCover.loading = loading.getSelectedItem().toString();
-                    healthCover.specialistAndTest = specialist.isChecked();
-                    mBenefits.healthCover = healthCover;
-                    if (mBenefitsCallback != null){
-                        mBenefitsCallback.onHealthCoverUpdate(mBenefits.healthCover);
-                    }
-                    break;
-                case R.id.fbLifeCover:
-                    LifeCover lifeCover = new LifeCover();
-                    lifeCover.coverAmount = coverAmount.getRawValue();
-                    lifeCover.futureInsurability = futureInsurability.isChecked();
-                    lifeCover.index = index.isChecked();
-                    lifeCover.loading = loading.getSelectedItem().toString();
-                    lifeCover.yearlyRenewable = renewable.getSelectedItem().toString();
-                    mBenefits.lifeCover = lifeCover;
-                    if (mBenefitsCallback != null){
-                        mBenefitsCallback.onLifeCoverUpdate(mBenefits.lifeCover);
-                    }
-                    break;
-            }
-        });
 
         switch (fancyButton.getId()) {
             case R.id.fbHealthCover:
@@ -228,7 +143,73 @@ public class BenefitsPresenter {
                     }
                 }
                 break;
+            default:
+                builder.setNegativeButton("Close", (dialog, which) -> {});
+                break;
         }
+        if (dialogView != null) {
+            FancyButton applyChanges = dialogView.findViewById(R.id.btnApplyChanges);
+            applyChanges.setOnClickListener(v -> {
+                updateResources(activity, fancyButton, true, benefitIcons);
+                switch (fancyButton.getId()) {
+                    case R.id.fbHealthCover:
+                        HealthCover healthCover = new HealthCover();
+                        healthCover.excess = excess.getSelectedItem().toString();
+                        healthCover.dentalAndOptical = dental.isChecked();
+                        healthCover.gpAndPrescriptions = prescription.isChecked();
+                        healthCover.loading = loading.getSelectedItem().toString();
+                        healthCover.specialistAndTest = specialist.isChecked();
+                        mBenefits.healthCover = healthCover;
+                        if (mBenefitsCallback != null) {
+                            mBenefitsCallback.onHealthCoverUpdate(mBenefits.healthCover);
+                        }
+                        break;
+                    case R.id.fbLifeCover:
+                        LifeCover lifeCover = new LifeCover();
+                        lifeCover.coverAmount = coverAmount.getRawValue();
+                        lifeCover.futureInsurability = futureInsurability.isChecked();
+                        lifeCover.index = index.isChecked();
+                        lifeCover.loading = loading.getSelectedItem().toString();
+                        lifeCover.yearlyRenewable = renewable.getSelectedItem().toString();
+                        mBenefits.lifeCover = lifeCover;
+                        if (mBenefitsCallback != null) {
+                            mBenefitsCallback.onLifeCoverUpdate(mBenefits.lifeCover);
+                        }
+                        break;
+                }
+                mAlertDialog.dismiss();
+            });
+            FancyButton remove = dialogView.findViewById(R.id.btnRemove);
+            remove.setOnClickListener(v -> {
+                AlertDialog.Builder confirmation = new AlertDialog.Builder(activity);
+                confirmation.setTitle("Are you sure?");
+                confirmation.setIcon(activity.getResources().getDrawable(R.drawable.ic_error));
+                confirmation.setMessage("You cannot undo this.");
+                confirmation.setNegativeButton("No", (dialog, which) -> {
+                });
+                confirmation.setPositiveButton("Yes", (dialog1, which1) -> {
+                    updateResources(activity, fancyButton, false, benefitIcons);
+                    switch (fancyButton.getId()) {
+                        case R.id.fbHealthCover:
+                            mBenefits.healthCover = null;
+                            if (mBenefitsCallback != null) {
+                                mBenefitsCallback.onHealthCoverUpdate(null);
+                            }
+                            break;
+                        case R.id.fbLifeCover:
+                            mBenefits.lifeCover = null;
+                            if (mBenefitsCallback != null) {
+                                mBenefitsCallback.onLifeCoverUpdate(null);
+                            }
+                            break;
+                    }
+                    mAlertDialog.dismiss();
+                }).show();
+            });
+            ImageView close = dialogView.findViewById(R.id.ivClose);
+            close.setOnClickListener(v -> mAlertDialog.dismiss());
+        }
+
     }
 
     private void updateResources(Activity activity, FancyButton fancyButton,
@@ -255,7 +236,7 @@ public class BenefitsPresenter {
         return spinnerArrayAdapter;
     }
 
-    public void setCallback(BenefitsCallback benefitsCallback){
+    public void setCallback(BenefitsCallback benefitsCallback) {
         mBenefitsCallback = benefitsCallback;
     }
 }
